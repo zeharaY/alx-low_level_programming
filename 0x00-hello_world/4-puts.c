a4c40d05c36727e230c77c16c5308479217703ae
zeharaY <yassinzehara@gmail.com>
2022-06-30 15:58:58 -0700

    com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+     * main - main block
+     *  of the code with return value of 0
+     *   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
