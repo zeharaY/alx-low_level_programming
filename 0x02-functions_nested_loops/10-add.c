6e6c0a8d4a3f4016e15fc82f97ffea8e22a75473
zeharaY <yassinzehara@gmail.com>
2022-07-06 05:46:44 -0700

    com

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ * @a : parameter
+ * @b : parameter
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
