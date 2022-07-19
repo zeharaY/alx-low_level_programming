6f099972f60b437f242e64610bef987585923b3b
zeharaY <yassinzehara@gmail.com>
2022-07-19 01:42:00 -0700

    re commit this

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,24 +7,25 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
-		unsigned int c = 0;
-		char *t = accept;
+	unsigned int c = 0;
+	char *t = accept;
 
-				while (*s++)
+	while (*s++)
 
-						{
+	{
+		while (*accept++)
 
-									while (*accept++)
-													if (*(s - 1) == *(accept - 1))
+			if (*(s - 1) == *(accept - 1))
 
-																	{
-																						c++;
-																										break;
-																													}
-											if (!(*--accept))
+			{
+				c++;
 
-															break;
-													accept = t;
-														}
-					return (c);
+				break;
+			}
+		if (!(*--accept))
+
+			break;
+		accept = t;
+	}
+	return (c);
 }
