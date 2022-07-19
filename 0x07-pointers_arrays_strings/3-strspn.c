b30414eef3e2958566dfd0aa3619541672ecaf8a
zeharaY <yassinzehara@gmail.com>
2022-07-19 01:37:11 -0700

    com

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: String where substring will look.
+ * @accept: chars to be checked/substring.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+		unsigned int c = 0;
+		char *t = accept;
+
+				while (*s++)
+
+						{
+
+									while (*accept++)
+													if (*(s - 1) == *(accept - 1))
+
+																	{
+																						c++;
+																										break;
+																													}
+											if (!(*--accept))
+
+															break;
+													accept = t;
+														}
+					return (c);
+}
