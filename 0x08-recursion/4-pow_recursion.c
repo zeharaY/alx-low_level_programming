42a318574de26f891c8601095a74aac67f98f4a5
zeharaY <yassinzehara@gmail.com>
2022-07-20 05:18:50 -0700

    com

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - pow recursion
+ * @x: integer parameter
+ * @y: integer parameter
+ *
+ * Return: recursion
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+
+	{
+		return (-1);
+	}
+	if (y == 0)
+
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
